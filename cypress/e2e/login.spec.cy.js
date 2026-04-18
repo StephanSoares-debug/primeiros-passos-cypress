3925807e9ca451ff73a70a86761ca7dc3f12b905
Stephan Soares <soares.stephan@gmail.com>
2026-04-18 14:39:13 -0300

    datefield added to project]

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,21 +14,29 @@ describe('Orange HRM Tests', () => {
     FirstNameField: '[name="firstName"]',
     MiddleNameField: '[name="middleName"]',
     LastNameField: '[name="lastName"]',
-    EmployeeIDField: '.oxd-input--active'
+    GenericField: '.oxd-input--active',
+    dateField: '[placeholder="yyyy-dd-mm"]',
+    SaveButton: '[type="submit"]'
 
   }
  
   it.only('Login - Success and update Myinfo', () => {
 
     cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(UserData.UserSucess.username)
-    cy.get(selectorsList.passwordField).type(UserData.UserSucess.password)
+    cy.get(selectorsList.usernameField).clear().type(UserData.UserSucess.username)
+    cy.get(selectorsList.passwordField).clear().type(UserData.UserSucess.password)
     cy.get(selectorsList.loginbutton).click()
     cy.location('pathname').should('equals', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboadGrid)
     cy.get(selectorsList.MyinfoButtom).click()
-    cy.get(selectorsList.MiddleNameField).type('Silva')
-    cy.get(selectorsList.EmployeeIDField).eq(4).type('user999')
+    cy.get(selectorsList.MiddleNameField).clear().type('Silva')
+    cy.get(selectorsList.GenericField).eq(3).clear().type('NickNameID')
+    cy.get(selectorsList.GenericField).eq(4).clear().type('EmployID12')
+    cy.get(selectorsList.GenericField).eq(5).clear().type('OtherID123')
+    cy.get(selectorsList.GenericField).eq(6).clear().type('DriverLicenceNumber123')
+    cy.get(selectorsList.dateField).eq(0).clear().type("2026-18-04").click()
+    cy.get(selectorsList.SaveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
 
   })
 
